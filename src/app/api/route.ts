7b766ae82dc963fc09bbcc4fcba0b54fc3302862
Mohit Giri <mohitgiri1103@gmail.com>
2024-04-28 02:59:21 +0530

    Try vercel/postgres

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,16 +1,26 @@
 import { NodeNextResponse } from "next/dist/server/base-http/node";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({
-    project: "DBMS By Hema Ma'am",
-  });
-}
-
+// export async function GET() {
+//   return NextResponse.json({
+//     project: "DBMS By Hema Ma'am",
+//   });
+// }
 
 export async function POST(request: Request) {
   const data = await request.json();
   return NextResponse.json({
     data,
   });
+}
+
+import { sql } from "@vercel/postgres";
+
+export async function GET(request: NextRequest) {
+  try {
+    const result = await sql`select * from voter`;
+    return NextResponse.json({ result: result.rows }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
